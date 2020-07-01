d6e96bb23f248f6d88b446f6042265f015ea428c
Jørgen Tellnes <jorgen@arkitektum.no>
2020-07-01 15:45:11 +0200

    Add a static test user to the user database seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -11,6 +12,12 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name' => 'Siegfried Roy',
+            'email' => 'siegfried@roy.test',
+            'password' => '$2y$10$iHG5PAcbkD7ml8X7g9C0FeprfGl/1mxJC/Zo.b6NUEsprfxG57mpW' // arkadeweb
+        ]);
+
         factory(App\User::class, 5)->create();
     }
 }
