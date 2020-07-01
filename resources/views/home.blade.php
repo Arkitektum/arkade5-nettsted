6babe8b06c5d32ece99c2b125ef85e4b20045bc3
Jørgen Tellnes <jorgen@arkitektum.no>
2020-07-01 16:14:38 +0200

    Setup control panel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Kontrollpanel') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <div class="navbar">
+                            <div class="navbar-text">Nedlastingsstatistikk</div>
+                            <a class="page-link" title="Statistikk" href="{{ route('statistics.index') }}">JSON</a>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
