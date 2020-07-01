a9edb2f0affd0008236b66257bd736e38c4d08eb
Jørgen Tellnes <jorgen@arkitektum.no>
2020-07-01 15:36:48 +0200

    Use conventional file name format in the Arkade release database seeder

--- database/seeds/ArkadeReleaseSeeder.php
+++ database/seeds/ArkadeReleaseSeeder.php
@@ -15,42 +15,42 @@ class ArkadeReleaseSeeder extends Seeder
         ArkadeRelease::create([
             'version_number' => '2.0.0',
             'user_interface' => 'GUI',
-            'package_filename' => 'Arkade5_GUI_2.0.0.msi',
+            'package_filename' => 'Arkade5-2.0.0.msi',
             'released_at' => '2020-07-08'
         ]);
 
         ArkadeRelease::create([
             'version_number' => '2.0.0',
             'user_interface' => 'CLI',
-            'package_filename' => 'Arkade5_CLI_2.0.0.zip',
+            'package_filename' => 'Arkade5CLI-2.0.0.zip',
             'released_at' => '2020-07-08'
         ]);
 
         ArkadeRelease::create([
             'version_number' => '2.1.0',
             'user_interface' => 'GUI',
-            'package_filename' => 'Arkade5_GUI_2.1.0.msi',
+            'package_filename' => 'Arkade5-2.1.0.msi',
             'released_at' => '2020-10-19'
         ]);
 
         ArkadeRelease::create([
             'version_number' => '2.1.0',
             'user_interface' => 'CLI',
-            'package_filename' => 'Arkade5_CLI_2.1.0.zip',
+            'package_filename' => 'Arkade5CLI-2.1.0.zip',
             'released_at' => '2020-10-19'
         ]);
 
         ArkadeRelease::create([
             'version_number' => '2.2.0',
             'user_interface' => 'GUI',
-            'package_filename' => 'Arkade5_GUI_2.2.0.msi',
+            'package_filename' => 'Arkade5-2.2.0.msi',
             'released_at' => '2021-01-15'
         ]);
 
         ArkadeRelease::create([
             'version_number' => '2.2.0',
             'user_interface' => 'CLI',
-            'package_filename' => 'Arkade5_CLI_2.2.0.zip',
+            'package_filename' => 'Arkade5CLI-2.2.0.zip',
             'released_at' => '2021-01-15'
         ]);
     }
