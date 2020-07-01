1b5277a4d40dd07b7bf3fa47e5fefd804c03506f
Jørgen Tellnes <jorgen@arkitektum.no>
2020-07-01 17:54:49 +0200

    Process API Arkade download post requests

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 
+use App\Organization;
+use App\ArkadeDownloader;
+use App\ArkadeDownload;
+use App\ArkadeRelease;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,3 +36,44 @@ Route::get('/', function () {
         ],
     ];
 })->name('index');
+
+Route::post('arkade-downloads', function (Request $request) {
+
+    $arkadeUI = $request->input('arkadeUI');
+    $latestReleaseForUI = ArkadeRelease::whereUserInterface($arkadeUI)->orderByDesc('released_at')->first();
+    $release = ArkadeRelease::find($latestReleaseForUI->id);
+
+    $filename = $release->package_filename;
+    $headers = ['Filename' => $filename, 'Access-Control-Expose-Headers' => 'Filename'];
+
+    if(!$arkadePackageFile = Storage::download($filename, $filename, $headers))
+        abort(500, 'Download failed');
+
+    $arkadeDownload = new ArkadeDownload();
+    $arkadeDownload->arkadeRelease()->associate($release);
+    $arkadeDownloader = ArkadeDownloader::updateOrCreate(
+        ['email' => $request->input('downloaderEmail')],
+        [
+            'has_arkade_v1_experience' => $request->input('downloaderA1Xp'),
+            'wants_news' => $request->input('downloaderNews'),
+        ]);
+    $arkadeDownload->arkadeDownloader()->associate($arkadeDownloader);
+
+    if ($orgNumber = $request->input('orgNumber')) {
+        $organization = Organization::updateOrCreate(
+            ['org_number' => $orgNumber],
+            [
+                'name' => $request->input('orgName'),
+                'org_form' => $request->input('orgForm'),
+                'address' => $request->input('orgAddress'),
+                'latitude' => $request->input('orgLatitude'),
+                'longitude' => $request->input('orgLongitude')
+            ]
+        );
+        $arkadeDownload->organization()->associate($organization);
+    }
+    $arkadeDownload->save();
+
+    return $arkadePackageFile;
+
+})->name('download.store');
